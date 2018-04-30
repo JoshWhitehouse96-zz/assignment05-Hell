8764b7e1840a91c30188aa095a536f3e0e63de4e
Noah Gorstein <ngorstein@gmail.com>
2018-04-30 00:47:24 -0400

    finishing touches

--- login.php
+++ login.php
@@ -5,8 +5,9 @@
 <div class="jumbotron paral paralsec3 flexBox">
   <h1 class="display-2"> Sign In</h1>
     <p class="lead">
-      <form action="" method="post">
-        <input type="email" name="email" id="email" placeholder="email">
+      <form action="" method="post" class="flexBox">
+        <input id="emailInput" type="email" name="email" id="email" placeholder="email">
+        <button class="btn btn-outline-success btn-lg" type="submit" value="login">Sign Up </button>
       </form>
     </p>
 </div>
--- signUp.php
+++ signUp.php
@@ -8,8 +8,8 @@
 <div class="jumbotron paral paralsec3 flexBox">
   <h1 class="display-3"> Sign Up to learn More!</h1>
     <p class="lead">
-      <form action="" method="post">
-        <input id="signUpInput" type="email" name="email" id="email" placeholder="email">
+      <form action="" method="post" class="flexBox">
+        <input id="signUpInput" type="email" name="email" id="email" placeholder="email" autofocus="true">
         <button class="btn btn-outline-success btn-lg" type="submit" value="Sign-Up">Sign Up </button>
       </form>
     </p>
